8814f9a17a7159d4ae38601917cbc651d0a29681
Asish <Asish@Asish-PC>
2018-12-10 18:01:34 +0530

    gggg

--- TestPoject/src/com/genericlib/common/Demoa.java
+++ TestPoject/src/com/genericlib/common/Demoa.java
@@ -4,8 +4,9 @@ public class Demoa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("My   Projecthhggh");
+		System.out.println("My Projecthhggh");
 
+		System.out.println("abc");
 	}
 
 }
