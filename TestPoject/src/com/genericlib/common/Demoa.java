d14987ee87f3de2bd8ed47cc961c6aa6aa15ce02
Asish <Asish@Asish-PC>
2018-12-11 13:59:04 +0530

    My First Commit

--- TestPoject/src/com/genericlib/common/Demoa.java
+++ TestPoject/src/com/genericlib/common/Demoa.java
@@ -3,10 +3,14 @@ package com.genericlib.common;
 public class Demoa {
 
 	public static void main(String[] args) {
-		
-		System.out.println("My Projecthhggh");
 
-		System.out.println("abc");
+		System.out.println("My Projecthhggh");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+
+		System.out.println(c);
 	}
 
 }
