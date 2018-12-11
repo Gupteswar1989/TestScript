735818760a69ced1d1084fe97abeaa6f4f5cb3c0
Asish <Asish@Asish-PC>
2018-12-11 14:49:50 +0530

    no change

--- TestPoject/src/com/genericlib/common/Demoa.java
+++ TestPoject/src/com/genericlib/common/Demoa.java
@@ -10,10 +10,7 @@ public class Demoa {
 		int c=a+b;
 
 
-		System.out.println(c);
-		int a = 10;
-		int b = 20;
-		int c = a + b;
+		
 
 		System.out.println(c);
 		System.out.println("This is My Programming");
