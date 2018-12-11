42a5c5816773470d3c503002c0d69ead108f5db4
Asish <Asish@Asish-PC>
2018-12-11 14:04:36 +0530

    I have done the chaneges

--- TestPoject/src/com/genericlib/common/Demoa.java
+++ TestPoject/src/com/genericlib/common/Demoa.java
@@ -3,15 +3,15 @@ package com.genericlib.common;
 public class Demoa {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("My Projecthhggh");
-		
-		int a=10;
-		int b=20;
-		int c=a+b;
-		
+
+		int a = 10;
+		int b = 20;
+		int c = a + b;
 
 		System.out.println(c);
+		System.out.println("This is My Programming");
 	}
 
 }
