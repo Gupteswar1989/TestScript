9ff904feedcde10ba551f925e07f849041ecffc1
Asish <Asish@Asish-PC>
2018-12-11 12:38:01 +0530

    write a program for addition

--- TestPoject/src/com/genericlib/common/Demoa.java
+++ TestPoject/src/com/genericlib/common/Demoa.java
@@ -5,8 +5,13 @@ public class Demoa {
 	public static void main(String[] args) {
 		
 		System.out.println("My Projecthhggh");
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
 
-		System.out.println("abc");
+		System.out.println(c);
 	}
 
 }
