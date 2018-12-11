f88305c89b09624ee19bff8761f94c87f96a03b4
Asish <Asish@Asish-PC>
2018-12-11 14:54:31 +0530

    added new Line

--- TestPoject/src/com/genericlib/common/Demoa.java
+++ TestPoject/src/com/genericlib/common/Demoa.java
@@ -5,20 +5,19 @@ public class Demoa {
 	public static void main(String[] args) {
 
 		System.out.println("My Projecthhggh");
-		int a=10;
-		int b=20;
-		int c=a+b;
-
-
-		
+		int a = 10;
+		int b = 20;
+		int c = a + b;
 
 		System.out.println(c);
 		System.out.println("This is My Programming");
 		int a1;
-		a1=10;
-		
+		a1 = 10;
+
 		System.out.println(a1);
 		System.out.println("***********************");
+
+		System.out.println("Myname is the Chiku");
 	}
 
 }
